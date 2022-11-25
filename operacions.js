22b7844790976e3d86d283352b0ef03d87b8b474
AinaraProgram <115171219+AinaraProgram@users.noreply.github.com>
2022-11-25 09:25:55 +0100

    Update operacions.js

--- operacions.js
+++ operacions.js
@@ -1,41 +1,127 @@
-function agregar(){
-    let varNom = prompt("Indica el nom de la imatge: ", "Boku no Hero");
-    let index = buscar(varNom);
-
-    if (index < 0){
-        let varImatge = prompt("Indica el nom de la imatge jpg (sense l'extensió): ", "boku no hero");
-        let varNom = prompt("Indica el nom que li posaràs a la imatge: ", "Boku no Hero Academia");
-        let varAutor = prompt("Indica l'autor de la imatge: ", "Horikoshi Kōhei");
-        let varDescripcio = prompt("Escriu una breu descripció: ", "Boku no Hero Academia és un manga molt popular tant al Japó com a la resta del món.");
-
-        const imatgeInsercio = 
-        {
-            imatge: varImatge,
-            nom: varNom,
-            autor: varAutor,
-            descripcio: varDescripcio,
-        }
-        imatges.push(imatgeInsercio);
-        posicioActual = imatges.length -1;
-        mostraImatge();
-    } else {
-        alert("La imatge ja existeix!");
-        posicioActual = index;
-    }
+body {   
+   margin: auto;
+   font-family: 'Nunito', 'Open Sans';
+   background-image: url("../img/fons_background.png");
+   background-attachment: fixed;  
 }
 
-function buscar(valor){
-    let trobat = false;
-    let index = -1;
-    let i = 0;
-    let voltes = imatges.length;
+header {
+   position: fixed; 
+   top: 0;
+   width: fit-content;
+   background-color: rgb(104, 104, 104); 
+   padding-top: 0.10em;
+}
 
-    while (i < voltes && !trobat){
-        if(valor == imatges[i].nom){
-            index = i;
-            trobat = true;
-        }
-        i++;
-    }
-    return index;
+/* #chibiConan{
+   width: 30px;
+}
+
+#chibiSakura{
+   width: 30px;
+} */
+
+
+#tema {font-weight: lighter; margin-bottom: 0px; color: rgb(201, 201, 201); font-size: 40px; text-decoration: underline;}
+h2 {color: deeppink;}
+
+#nomImatge{
+   font-weight: bolder;
+}
+
+.repro {cursor: pointer; background:none; padding: 0px; margin-left: 2em; border: 0px;}
+
+#container {
+   width: fit-content;
+   margin: auto;
+   margin-top: 2em;
+   background-color: rgb(64, 64, 64);
+   padding-bottom: 0.5em;
+   text-align: center;
+   box-shadow: 3px 5px rgba(0, 0, 0, 0.187);
+}
+#container p, h1{
+   text-align: center;
+}
+
+#imatge {
+   width: 620px;
+   border: 15px solid lightsteelblue;
+}
+
+#nomImatge {
+   /* background-color: yellow; */
+   width: 20em;
+   text-align: center;
+   font-weight: bold;
+   font-size: 110%;
+   color: rgb(201, 201, 201);
+}
+
+#afegir{
+   cursor: pointer;
+   background-color: pink;
+   box-shadow: 3px 3px rgba(226, 226, 226, 0.187);
+}
+
+#esborrar{
+   cursor: pointer;
+   background-color: pink;
+   box-shadow: 3px 3px rgba(226, 226, 226, 0.187);
+}
+
+#modificar{
+   cursor: pointer;
+   background-color: pink;
+   box-shadow: 3px 3px rgba(226, 226, 226, 0.187);
+}
+
+#retrocedir{
+   cursor: pointer;
+   background-color: pink;
+   box-shadow: 3px 3px rgba(226, 226, 226, 0.187);
+}
+
+#avançar{
+   cursor: pointer;
+   background-color: pink;
+   box-shadow: 3px 3px rgba(226, 226, 226, 0.187);
+}
+
+#signat{
+   text-align: center;
+   width: 100%;
+   
+}
+
+#peu{
+   color: rgb(201, 201, 201);
+   background-color: rgb(64, 64, 64);
+
+}
+
+#descrip{
+   background-color: antiquewhite;
+   width: 600px;
+   padding: 10px;
+   margin: 0 auto;
+   margin-inline: 20px;
+   border: 15px solid rgb(222, 221, 176);
+
+}
+
+#nom{
+   color:brown;  
+   text-decoration: underline;
+   margin-bottom: -10px;
+   margin-top: -5px;
+}
+
+#subnom{
+   color: brown;
+   text-decoration: underline;
+}
+
+#desc{
+   text-align: justify !important;
 }
